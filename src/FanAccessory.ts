8d4d4c0e52b258a093ff3d19e86170ac31a5ca2c
Gavin Zyonse <gzyonse@gmail.com>
2023-04-23 17:43:20 -0400

    Re-add "Active" characteristic

--- src/FanAccessory.ts
+++ src/FanAccessory.ts
@@ -39,11 +39,30 @@ export class FanAccessory {
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.deviceName);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    // see https://developers.homebridge.io/#/service/Fanv2
+    // register handlers for the Active Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .onSet(this.handleActiveSet.bind(this))                // SET - bind to the `setOn` method below
+      .onGet(this.handleActiveGet.bind(this));               // GET - bind to the `getOn` method below
 
     // register handlers for the RotationSpeed Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.RotationSpeed)
       .onSet(this.setRotationSpeed.bind(this));       // SET - bind to the 'setBrightness` method below
+  }
+
+  // Handle requests to set the "Active" characteristic
+  handleActiveSet(value) {
+    this.platform.log.debug('Triggered SET Active:', value);
+  }
+
+  // Handle requests to get the current value of the "Active" characteristic
+  handleActiveGet() {
+    this.platform.log.debug('Triggered GET Active');
+
+    // set this to a valid value for Active
+    const currentValue = 1;
+
+    return currentValue;
   }
 
   /**
